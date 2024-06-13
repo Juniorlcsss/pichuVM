9af33f6c824e97070c0fdcf66dcd9a402f83d097
root <j.randall2@lancaster.ac.uk>
2024-06-13 23:47:37 +0100

    Functionality in all of the instructions in the set, added a stack print function to print all the non-null values in the stack

--- pichu.c
+++ pichu.c
@@ -4,13 +4,13 @@
 #include <assert.h>
 
 typedef enum{
-    INST_PUSH,
-    INST_POP,
-    INST_ADD,
-    INST_SUB,
-    INST_MUL,
-    INST_DIV,
-    INST_PRINT,
+    INST_PUSH,  
+    INST_POP,   
+    INST_ADD,   
+    INST_SUB,   
+    INST_MUL,   
+    INST_DIV,   
+    INST_PRINT, 
 } instructionSet;
 
 typedef struct{
@@ -20,8 +20,12 @@ typedef struct{
 
 #define PROGRAM_SIZE (sizeof(program)/sizeof(program[0]))
 instruction program[] = {
-    {.type = INST_PUSH, .operand = 15},
-    {.type = INST_PRINT},
+    {.type = INST_PUSH, .operand = 1},
+    {.type = INST_PUSH, .operand = 2},
+    {.type = INST_PUSH, .operand = 3},
+    {.type = INST_PUSH, .operand = 4},
+    //{.type = INST_DIV},
+    //{.type = INST_PRINT},
 };
 
 #define MAX_STACK_SIZE 1024
@@ -29,11 +33,26 @@ int stack[MAX_STACK_SIZE];
 int currentStackSize;
 
 void push(int value){   //push value
+    if(currentStackSize >= MAX_STACK_SIZE){
+        fprintf(stderr, "stack overflow\n");
+        exit(EXIT_FAILURE);
+    }
     stack[currentStackSize] = value;
     currentStackSize++;
 }
 int pop(){             //pop the top
+    if(currentStackSize <= 0) {
+        fprintf(stderr, "stack underflow\n");
+        exit(EXIT_FAILURE);
+    }
     return stack[--currentStackSize];
+}
+
+void printStack(){
+    for(int i= currentStackSize - 1; i>=0; i--){
+        printf("%d\n", stack[i]);
+    }
+    printf("\n");
 }
 
 int main(){
@@ -43,14 +62,23 @@ int main(){
                 push(program[i].operand);
                 break;
             case INST_POP:
+                pop();
                 break;
             case INST_ADD:
+                push(pop() + pop());
                 break;
             case INST_SUB:
+                int first = pop();
+                int second = pop();
+                push(second - first);
                 break;
             case INST_MUL:
+                push(pop() * pop());
                 break;
             case INST_DIV:
+                first = pop();
+                second = pop();
+                push(second / first);
                 break;
             case INST_PRINT:
                 printf("%d\n", pop());
@@ -60,5 +88,6 @@ int main(){
                 break;
         }
     }
+    printStack();
     return 0;
 }
