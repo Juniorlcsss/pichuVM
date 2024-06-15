ea5d3fc01cf25f43e34a3ce983ae6efac7a0d20e
root <j.randall2@lancaster.ac.uk>
2024-06-15 12:20:56 +0100

    Compiles, but i keep getting segmentation errors, try and fix this later, rooting as its been 22 hours since last one and would prefer a backup

--- pichu.c
+++ pichu.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <assert.h>
 
+//Constants
+#define MAX_STACK_SIZE 1024
 typedef enum{
     INST_PUSH,  
     INST_POP,   
@@ -13,11 +15,19 @@ typedef enum{
     INST_PRINT, 
 } instructionSet;
 
-
 typedef struct{
     instructionSet type;
     int operand;
 } instruction;
+
+typedef struct{
+    //where stack is stored
+    int stack[MAX_STACK_SIZE];
+    int currentStackSize;
+    size_t programSize;
+    //instruction program;
+    instruction *instructions[];
+} Machine;
 
 //macros
 #define M_INST_PUSH(x) {.type = INST_PUSH, .operand = x}//only one that needs value!
@@ -28,93 +38,121 @@ typedef struct{
 #define M_INST_DIV() {.type = INST_DIV}
 #define M_INST_PRINT() {.type = INST_PRINT}
 
-//running the instructions
-#define PROGRAM_SIZE (sizeof(program)/sizeof(program[0]))
 instruction program[] = {
-    M_INST_PUSH(1),
-    M_INST_PUSH(2),
-    M_INST_ADD(),
-    M_INST_PUSH(3),
-    M_INST_PUSH(4),
-    M_INST_SUB(),
-    M_INST_PUSH(5),
     M_INST_PUSH(6),
-    M_INST_MUL(),
-    M_INST_PUSH(100),
-    M_INST_PUSH(5),
-    M_INST_DIV(),
-    M_INST_PUSH(9),
-    M_INST_PUSH(10),
+    M_INST_PUSH(7),
     M_INST_ADD(),
-    M_INST_PUSH(11),
-    M_INST_PUSH(12),
-    M_INST_ADD(),
-    M_INST_PUSH(13),
-    M_INST_PUSH(14),
-    M_INST_ADD(),
-    M_INST_PUSH(15),
 };
+#define PROGRAM_SIZE (sizeof(program)/sizeof(program[0]))
 
-#define MAX_STACK_SIZE 1024
-int stack[MAX_STACK_SIZE];
-int currentStackSize;
-
-void push(int value){   //push value
-    if(currentStackSize >= MAX_STACK_SIZE){
+void push(Machine *machine, int operand){   //push value
+    if(machine->currentStackSize >= MAX_STACK_SIZE){
         fprintf(stderr, "stack overflow\n");
         exit(EXIT_FAILURE);
     }
-    stack[currentStackSize] = value;
-    currentStackSize++;
+    machine->stack[machine->currentStackSize] = operand;
+    machine->currentStackSize++;
 }
-int pop(){             //pop the top
-    if(currentStackSize <= 0) {
+int pop(Machine *machine){             //pop the top
+    if(machine->currentStackSize <= 0) {
         fprintf(stderr, "stack underflow\n");
         exit(EXIT_FAILURE);
     }
-    return stack[--currentStackSize];
+    machine->currentStackSize--;
+    return machine->stack[machine->currentStackSize];
 }
 
-void printStack(){
-    for(int i= currentStackSize - 1; i>=0; i--){
-        printf("%d\n", stack[i]);
+void printStack(Machine *machine){
+    printf("stack:\n");
+    for(int i = machine->currentStackSize - 1; i>=0; i--){
+        printf("%d\n", machine->stack[i]);
     }
-    printf("\n");
+    printf("------\n");
+}
+
+void writeProgramToFile(Machine *machine, char *filename){
+    FILE *f = fopen(filename, "wb");
+    if(f == NULL){
+        fprintf(stderr, "could not open file %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+    else{
+        fwrite(machine->instructions, sizeof(machine->instructions[0]), PROGRAM_SIZE, f);
+        printf("program written to file %s\n", filename);
+    }
+    fclose(f);
+}
+
+Machine *readFromFile(Machine *machine, char *filename){
+    FILE *f = fopen(filename,"r");
+    if(f == NULL){
+        fprintf(stderr, "could not open file %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+    else{
+        instruction *instructions = malloc(sizeof(instruction) * MAX_STACK_SIZE);
+        if (instructions == NULL) {
+            fprintf(stderr, "memory allocation failed\n");
+            exit(EXIT_FAILURE);
+        }
+
+        Machine *machine = malloc(sizeof(Machine));
+        if (machine == NULL) {
+            fprintf(stderr, "memory allocation failed\n");
+            exit(EXIT_FAILURE);
+        }
+
+        fseek(f, 0, SEEK_END);
+        int length = ftell(f);
+        fread(instructions, sizeof(instruction), length/sizeof(instruction), f);
+        printf("program read from file %s\n", filename);
+
+        machine->programSize = length/sizeof(instruction);
+        return machine;
+    }
+    fclose(f);
 }
 
 int main(){
-    for(size_t i=0; i< PROGRAM_SIZE; i++) {
-        switch(program[i].type){
+    int first;
+    int second;
+    Machine *loadedMachine = malloc(sizeof(Machine));
+    *loadedMachine->instructions = malloc(sizeof(program));
+    memcpy(loadedMachine->instructions, program, sizeof(program));
+    writeProgramToFile(loadedMachine, "program.bin");
+    loadedMachine = readFromFile(loadedMachine, "program.bin");
+    for(size_t i=0; i < loadedMachine->programSize; i++) {
+        switch((*loadedMachine->instructions)[i].type){
             case INST_PUSH:
-                push(program[i].operand);
+                push(loadedMachine, (*loadedMachine->instructions)[i].operand);
                 break;
             case INST_POP:
-                pop();
+                pop(loadedMachine);
                 break;
             case INST_ADD:
-                push(pop() + pop());
+                push(loadedMachine, pop(loadedMachine) + pop(loadedMachine));
                 break;
             case INST_SUB:
-                int first = pop();
-                int second = pop();
-                push(second - first);
+                first = pop(loadedMachine);
+                second = pop(loadedMachine);
+                push(loadedMachine, second - first);
                 break;
             case INST_MUL:
-                push(pop() * pop());
+                push(loadedMachine ,pop(loadedMachine) * pop(loadedMachine));
                 break;
             case INST_DIV:
-                first = pop();
-                second = pop();
-                push(second / first);
+                first = pop(loadedMachine);
+                second = pop(loadedMachine);
+                push(loadedMachine ,second / first);
                 break;
             case INST_PRINT:
-                printf("%d\n", pop());
+                printf("%d\n", pop(loadedMachine));
                 break;
             default://unexpected state
                 assert(0);
                 break;
         }
     }
-    printStack();
+    printStack(loadedMachine);
     return 0;
 }
